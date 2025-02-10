f2eec9344e547972b52dfd792e83c10a7c1ac190
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-02-10 10:30:18 -0500

    updated some stuff

--- web/js/home.js
+++ web/js/home.js
@@ -4,7 +4,7 @@ const section1Container = document.getElementById('section-1')
 mainContainer.addEventListener('scroll', () => {
     const rect = section1Container.getBoundingClientRect()
     const viewportHeight = window.innerHeight
-    const distanceFromTop = Math.max(0, rect.top)
+    const distanceFromTop = Math.max(0, rect.bottom)
     const darkness = 1 - Math.min(distanceFromTop / viewportHeight, 1)
     console.log(darkness)
     section1Container.style.backgroundColor = `rgba(0, 0, 0, ${darkness})`
