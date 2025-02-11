b39d3729c598869867462e188e6d486a068e8f32
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-02-11 10:42:37 -0500

    added way more stuff

--- web/js/home.js
+++ web/js/home.js
@@ -1,6 +1,16 @@
-// scroll effect to darken main image
+// elements
+const menuBurger = document.getElementById('burger-icon')
 const mainContainer = document.getElementById('main-container')
 const section1Container = document.getElementById('section-1')
+const menu = document.getElementById('menu')
+
+// functions
+const toggleMenu = () => {
+    menu.classList.toggle('open')
+    menuBurger.classList.toggle('active')
+}
+
+// event listeners
 mainContainer.addEventListener('scroll', () => {
     const rect = section1Container.getBoundingClientRect()
     const viewportHeight = window.innerHeight
@@ -9,9 +19,10 @@ mainContainer.addEventListener('scroll', () => {
     section1Container.style.backgroundColor = `rgba(0, 0, 0, ${darkness})`
 })
 
-// menu in-and-out effect
-const menuBurger = document.getElementById('logout')
-menuBurger.addEventListener('click', () => {
-    const menu = document.getElementById('menu')
-    menu.classList.toggle('open')
+menuBurger.addEventListener('click', toggleMenu)
+
+mainContainer.addEventListener('click', () => {
+    if (menu.classList.contains('open')) {
+        toggleMenu()
+    }
 })
