32a9700ada6af7365a0f6f5e861bba89c90a5033
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-06-02 08:37:45 -0400

    started working on HTML/CSS

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("yolo")
+	
+	mux := http.NewServeMux()
+
+	fs := http.StripPrefix("/web/", http.FileServer(http.Dir("web")))
+
+	mux.Handle("/web/", fs)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/html/index.html")
+	})
+
+	s := http.Server{
+		Addr: ":1337",
+		Handler: mux,
+	}
+
+	s.ListenAndServe()
 }
