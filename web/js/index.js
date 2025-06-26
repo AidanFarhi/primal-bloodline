ff6d6fa9a04f440e820ba4a8505041a605078422
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-06-26 08:55:16 -0400

    js refactor

--- web/js/index.js
+++ web/js/index.js
@@ -18,10 +18,8 @@ function touching(element1, element2) {
     const rect1 = element1.getBoundingClientRect()
     const rect2 = element2.getBoundingClientRect()
     return !(
-        rect1.right < rect2.left ||
-        rect1.left > rect2.right ||
-        rect1.bottom < rect2.top ||
-        rect1.top > rect2.bottom
+        rect1.right < rect2.left || rect1.left > rect2.right ||
+        rect1.bottom < rect2.top || rect1.top > rect2.bottom
     )
 }
 
