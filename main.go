d5c4c789d0d00376f64623fb351b6270d89c599e
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-07-03 08:38:40 -0400

    added templating

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("web/templates/layout.html", "web/templates/index.html")
+	t.Execute(w, nil)
+}
 
 func main() {
 	
@@ -10,9 +18,7 @@ func main() {
 
 	mux.Handle("/web/", fs)
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/html/index.html")
-	})
+	mux.HandleFunc("/", IndexHandler)
 
 	s := http.Server{
 		Addr: ":1337",
