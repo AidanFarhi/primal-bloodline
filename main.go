71b4e9ec2afa280a125e1b677447ba3d3632aea8
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-08-06 08:35:23 -0400

    started working on individual cat details pages

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"path"
+	"strings"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,8 +18,16 @@ func KittensHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func CatDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	name := strings.TrimPrefix(r.URL.Path, "/cat-details/")
+	name = path.Clean(name)
+	templatePath := fmt.Sprintf("web/templates/%s.html", name)
+	t, _ := template.ParseFiles("web/templates/layout.html", templatePath)
+	t.Execute(w, nil)
+}
+
 func main() {
-	
+
 	mux := http.NewServeMux()
 
 	fs := http.StripPrefix("/web/", http.FileServer(http.Dir("web")))
@@ -25,9 +36,10 @@ func main() {
 
 	mux.HandleFunc("/", IndexHandler)
 	mux.HandleFunc("/kittens", KittensHandler)
+	mux.HandleFunc("/cat-details/", CatDetailsHandler)
 
 	s := http.Server{
-		Addr: ":1337",
+		Addr:    ":1337",
 		Handler: mux,
 	}
 
