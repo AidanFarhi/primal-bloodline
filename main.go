c67bb8e1964054028859fc3f403a2b2ce0c42640
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-08-14 07:28:28 -0400

    did a bunch of refactoring

--- main.go
+++ main.go
@@ -9,12 +9,20 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("web/templates/layout.html", "web/templates/index.html")
+	t, _ := template.ParseFiles(
+		"web/templates/layout.html",
+		"web/templates/partials/main-nav.html",
+		"web/templates/index.html",
+	)
 	t.Execute(w, nil)
 }
 
 func KittensHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("web/templates/layout.html", "web/templates/kittens.html")
+	t, _ := template.ParseFiles(
+		"web/templates/layout.html",
+		"web/templates/partials/alternate-nav.html",
+		"web/templates/kittens.html",
+	)
 	t.Execute(w, nil)
 }
 
@@ -22,7 +30,11 @@ func CatDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	name := strings.TrimPrefix(r.URL.Path, "/cat-details/")
 	name = path.Clean(name)
 	templatePath := fmt.Sprintf("web/templates/%s.html", name)
-	t, _ := template.ParseFiles("web/templates/layout.html", templatePath)
+	t, _ := template.ParseFiles(
+		"web/templates/layout.html",
+		templatePath,
+		"web/templates/partials/alternate-nav.html",
+	)
 	t.Execute(w, nil)
 }
 
