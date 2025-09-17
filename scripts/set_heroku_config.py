30c90f6b32be95382937d3f552aa3c0f86eea662
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-09-16 20:53:11 -0400

    updated set_heroku_config script

--- scripts/set_heroku_config.py
+++ scripts/set_heroku_config.py
@@ -1,8 +1,8 @@
 import subprocess
 
 with open(".env", "r") as env_file:
-    for line in env_file.readlines():
-        key, val = line.strip().split("=")
-        subprocess.run(
-            f"heroku config:set {key}={val} --app primal-bloodline", shell=True
-        )
+    pairs = [line.strip() for line in env_file if line.strip() and "=" in line]
+
+all_pairs = " ".join(pairs)
+
+subprocess.run(f"heroku config:set {all_pairs} --app primal-bloodline", shell=True)
