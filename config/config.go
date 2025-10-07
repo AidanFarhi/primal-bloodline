fd9426c613614b3be91541d1515c903452576359
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-10-07 11:49:05 -0400

    updated env vars

--- config/config.go
+++ config/config.go
@@ -20,5 +20,5 @@ func (c *Config) Load() {
 	c.PrimalBloodlinePhoneNumber = os.Getenv("PHONE_NUMBER")
 	c.Port, _ = strconv.Atoi(os.Getenv("PORT"))
 	c.Develop, _ = strconv.ParseBool(os.Getenv("DEVELOP"))
-	c.JSONPath = "./data/kittens.json"
+	c.JSONPath = os.Getenv("JSON_PATH")
 }
